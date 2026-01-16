f2c2bd785b897eac1be1f2aee10eb0744f00ecee
VishaL6i9 <vishalkandakatla@gmail.com>
2026-01-16 15:20:30 +0530

    fix: add @AndroidEntryPoint annotation to MainActivity
    Activities hosting Hilt Fragments must be annotated with @AndroidEntryPoint
    to enable dependency injection. Without it, Hilt cannot inject dependencies
    into child Fragments.

--- app/src/main/kotlin/com/vishal/harpy/main/MainActivity.kt
+++ app/src/main/kotlin/com/vishal/harpy/main/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.vishal.harpy.R
 import com.vishal.harpy.features.network_monitor.presentation.ui.NetworkMonitorFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     
     override fun onCreate(savedInstanceState: Bundle?) {
