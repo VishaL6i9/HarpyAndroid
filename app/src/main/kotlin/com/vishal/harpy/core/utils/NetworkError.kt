51d8bb7b75fff41c624a1a399606b84cb8f2623b
VishaL6i9 <vishalkandakatla@gmail.com>
2026-01-16 14:44:53 +0530

    fix: resolve property override issue in NetworkError sealed class hierarchy

--- app/src/main/kotlin/com/vishal/harpy/core/utils/NetworkError.kt
+++ app/src/main/kotlin/com/vishal/harpy/core/utils/NetworkError.kt
@@ -3,32 +3,32 @@ package com.vishal.harpy.core.utils
 /**
  * Sealed class representing different types of network errors
  */
-sealed class NetworkError(val message: String, val errorCause: Throwable? = null) {
-    
+sealed class NetworkError(open val message: String, open val errorCause: Throwable? = null) {
+
     data class DeviceNotRootedError(override val errorCause: Throwable? = null) :
         NetworkError("Device is not rooted", errorCause)
-    
+
     data class NetworkScanError(override val errorCause: Throwable? = null) :
         NetworkError("Error scanning network", errorCause)
-    
+
     data class BlockDeviceError(override val errorCause: Throwable? = null) :
         NetworkError("Error blocking device", errorCause)
-    
+
     data class UnblockDeviceError(override val errorCause: Throwable? = null) :
         NetworkError("Error unblocking device", errorCause)
-    
+
     data class NetworkAccessError(override val errorCause: Throwable? = null) :
         NetworkError("Network access error", errorCause)
-    
+
     data class CommandExecutionError(override val errorCause: Throwable? = null) :
         NetworkError("Error executing network command", errorCause)
-    
+
     data class InvalidIpAddressError(val ipAddress: String) :
         NetworkError("Invalid IP address: $ipAddress")
-    
+
     data class InvalidMacAddressError(val macAddress: String) :
         NetworkError("Invalid MAC address: $macAddress")
-    
+
     data class UnknownError(override val errorCause: Throwable? = null) :
         NetworkError("Unknown error occurred", errorCause)
 }
