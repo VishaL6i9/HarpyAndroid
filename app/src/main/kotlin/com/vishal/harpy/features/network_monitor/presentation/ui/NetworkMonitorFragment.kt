68757598e814d0f69368d6080a3171facd37f1cb
VishaL6i9 <vishalkandakatla@gmail.com>
2026-01-16 16:14:00 +0530

    fix: add missing imports to NetworkMonitorFragment
    Added missing Android widget imports:
    - android.widget.LinearLayout
    - android.widget.TextView
    
    Organized imports alphabetically for better readability.

--- app/src/main/kotlin/com/vishal/harpy/features/network_monitor/presentation/ui/NetworkMonitorFragment.kt
+++ app/src/main/kotlin/com/vishal/harpy/features/network_monitor/presentation/ui/NetworkMonitorFragment.kt
@@ -4,20 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.vishal.harpy.R
 import com.vishal.harpy.core.utils.NetworkDevice
 import com.vishal.harpy.features.network_monitor.presentation.viewmodel.NetworkMonitorViewModel
-import kotlinx.coroutines.launch
-import androidx.recyclerview.widget.LinearLayoutManager
-import android.widget.Button
-import android.widget.ProgressBar
-import androidx.recyclerview.widget.RecyclerView
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class NetworkMonitorFragment : Fragment() {
