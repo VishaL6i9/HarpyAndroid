f5db497f0f564a6ff270226bc4c82a27661e3650
VishaL6i9 <vishalkandakatla@gmail.com>
2026-01-16 16:27:37 +0530

    feat: enable immersive mode with auto-hiding status and navigation bars

--- app/src/main/kotlin/com/vishal/harpy/main/MainActivity.kt
+++ app/src/main/kotlin/com/vishal/harpy/main/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.vishal.harpy.main
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.vishal.harpy.R
 import com.vishal.harpy.features.network_monitor.presentation.ui.NetworkMonitorFragment
@@ -13,10 +14,30 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         
+        enableImmersiveMode()
+        
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, NetworkMonitorFragment())
                 .commitNow()
         }
+    }
+    
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            enableImmersiveMode()
+        }
+    }
+    
+    private fun enableImmersiveMode() {
+        window.decorView.systemUiVisibility = (
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+        )
     }
 }
