2eb39ff77cb73388022b9ab73ad731e7eb804408
VishaL6i9 <vishalkandakatla@gmail.com>
2026-01-18 13:17:14 +0530

    feat(core): initialize logging on app startup
    - initialize LogUtils and start logcat capture in HarpyApplication
    - clear log buffer at app start for clean logging session

--- app/src/main/kotlin/com/vishal/harpy/HarpyApplication.kt
+++ app/src/main/kotlin/com/vishal/harpy/HarpyApplication.kt
@@ -1,6 +1,7 @@
 package com.vishal.harpy
 
 import android.app.Application
+import com.vishal.harpy.core.utils.LogUtils
 import com.vishal.harpy.core.utils.VendorLookup
 import dagger.hilt.android.HiltAndroidApp
 
@@ -9,5 +10,8 @@ class HarpyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         VendorLookup.initialize(this)
+        LogUtils.initialize(this)
+        LogUtils.startLogcatCapture(this)
+        LogUtils.clearLogBufferAtStart()
     }
 }
