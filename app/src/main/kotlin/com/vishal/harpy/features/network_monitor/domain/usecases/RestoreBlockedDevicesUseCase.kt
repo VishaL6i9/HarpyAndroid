55d9b2697e367582e51d5ae88e7aa225c707a145
VishaL6i9 <vishalkandakatla@gmail.com>
2026-02-07 21:53:56 +0530

    feat(network): Add RestoreBlockedDevicesUseCase

---
+++ app/src/main/kotlin/com/vishal/harpy/features/network_monitor/domain/usecases/RestoreBlockedDevicesUseCase.kt
@@ -0,0 +1,14 @@
+package com.vishal.harpy.features.network_monitor.domain.usecases
+
+import com.vishal.harpy.features.network_monitor.data.repository.NetworkMonitorRepository
+import com.vishal.harpy.core.utils.NetworkDevice
+import com.vishal.harpy.core.utils.NetworkResult
+import javax.inject.Inject
+
+class RestoreBlockedDevicesUseCase @Inject constructor(
+    private val repository: NetworkMonitorRepository
+) {
+    suspend operator fun invoke(devices: List<NetworkDevice>): NetworkResult<Int> {
+        return repository.restoreBlockedDevices(devices)
+    }
+}
