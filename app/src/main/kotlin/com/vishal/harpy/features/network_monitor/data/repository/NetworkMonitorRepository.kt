9d685b824760056165c807fc36d757ae0d8883bd
VishaL6i9 <vishalkandakatla@gmail.com>
2026-02-07 21:53:55 +0530

    refactor(network): Remove spoofing methods from NetworkMonitorRepository interface

--- app/src/main/kotlin/com/vishal/harpy/features/network_monitor/data/repository/NetworkMonitorRepository.kt
+++ app/src/main/kotlin/com/vishal/harpy/features/network_monitor/data/repository/NetworkMonitorRepository.kt
@@ -14,21 +14,4 @@ interface NetworkMonitorRepository {
     suspend fun testPing(device: NetworkDevice): NetworkResult<Boolean>
     fun isDeviceBlocked(ipAddress: String): Boolean
     suspend fun restoreBlockedDevices(devices: List<NetworkDevice>): NetworkResult<Int>
-
-    // DNS Spoofing methods
-    suspend fun startDNSSpoofing(domain: String, spoofedIP: String, interfaceName: String): NetworkResult<Boolean>
-    suspend fun stopDNSSpoofing(domain: String): NetworkResult<Boolean>
-    fun isDNSSpoofingActive(domain: String): Boolean
-
-    // DHCP Spoofing methods
-    suspend fun startDHCPSpoofing(
-        interfaceName: String,
-        targetMacs: Array<String>,
-        spoofedIPs: Array<String>,
-        gatewayIPs: Array<String>,
-        subnetMasks: Array<String>,
-        dnsServers: Array<String>
-    ): NetworkResult<Boolean>
-    suspend fun stopDHCPSpoofing(): NetworkResult<Boolean>
-    fun isDHCPSpoofingActive(): Boolean
 }
