71461fac111e720051f2d48dc7dfc23630dc3fb3
VishaL6i9 <vishalkandakatla@gmail.com>
2026-04-06 03:23:24 +0530

    feat: update BlockDeviceUseCase to support interface selection

--- app/src/main/kotlin/com/vishal/harpy/features/network_monitor/domain/usecases/BlockDeviceUseCase.kt
+++ app/src/main/kotlin/com/vishal/harpy/features/network_monitor/domain/usecases/BlockDeviceUseCase.kt
@@ -8,7 +8,7 @@ import javax.inject.Inject
 class BlockDeviceUseCase @Inject constructor(
     private val repository: NetworkMonitorRepository
 ) {
-    suspend operator fun invoke(device: NetworkDevice): NetworkResult<Boolean> {
-        return repository.blockDevice(device)
+    suspend operator fun invoke(device: NetworkDevice, interfaceName: String? = null): NetworkResult<Boolean> {
+        return repository.blockDevice(device, interfaceName)
     }
 }
