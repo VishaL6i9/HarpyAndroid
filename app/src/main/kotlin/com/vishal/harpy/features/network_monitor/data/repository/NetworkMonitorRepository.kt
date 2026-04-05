33321786e8c5a8809bc69047abfd0777c935bc2a
VishaL6i9 <vishalkandakatla@gmail.com>
2026-04-06 03:23:23 +0530

    feat: add getActiveInterface and interface parameters to repository interface

--- app/src/main/kotlin/com/vishal/harpy/features/network_monitor/data/repository/NetworkMonitorRepository.kt
+++ app/src/main/kotlin/com/vishal/harpy/features/network_monitor/data/repository/NetworkMonitorRepository.kt
@@ -5,13 +5,14 @@ import com.vishal.harpy.core.utils.NetworkTopology
 import com.vishal.harpy.core.utils.NetworkResult
 
 interface NetworkMonitorRepository {
-    suspend fun scanNetwork(): NetworkResult<List<NetworkDevice>>
+    suspend fun scanNetwork(interfaceName: String? = null): NetworkResult<List<NetworkDevice>>
     suspend fun isDeviceRooted(): NetworkResult<Boolean>
-    suspend fun blockDevice(device: NetworkDevice): NetworkResult<Boolean>
-    suspend fun unblockDevice(device: NetworkDevice): NetworkResult<Boolean>
-    suspend fun unblockAllDevices(): NetworkResult<Int>
-    suspend fun mapNetworkTopology(): NetworkResult<NetworkTopology>
+    suspend fun blockDevice(device: NetworkDevice, interfaceName: String? = null): NetworkResult<Boolean>
+    suspend fun unblockDevice(device: NetworkDevice, interfaceName: String? = null): NetworkResult<Boolean>
+    suspend fun unblockAllDevices(interfaceName: String? = null): NetworkResult<Int>
+    suspend fun mapNetworkTopology(interfaceName: String? = null): NetworkResult<NetworkTopology>
     suspend fun testPing(device: NetworkDevice): NetworkResult<Boolean>
     fun isDeviceBlocked(ipAddress: String): Boolean
-    suspend fun restoreBlockedDevices(devices: List<NetworkDevice>): NetworkResult<Int>
+    suspend fun restoreBlockedDevices(devices: List<NetworkDevice>, interfaceName: String? = null): NetworkResult<Int>
+    fun getActiveInterface(): String?
 }
